996df0492095b936404fa4b83627bc7a14cbaf60
Stephen Burke <sburke@us-sburke-mbp.local>
2016-10-05 17:08:32 -0400

    some small changes

--- chef_server/chefserver.sh
+++ chef_server/chefserver.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Run this as root or a sudo'er - Stephen Burke
+# Run this as root or a sudo'er - Stephen Burke 
 
 USER=admin
 PW=1234Easy
-ORG1=devops
-ORG2="DevOps Test Org"
+ORG1=devops # simple org name
+ORG2="DevOps Test Org" # full org name 
+EMAIL=steburke71@hotmail.com
+FIRSTNAME=Stephen
+LASTNAME=Burke
+
 ufw disable
 URL='https://packages.chef.io/stable/ubuntu/16.04/chef-server-core_12.9.1-1_amd64.deb'
 
@@ -15,7 +19,7 @@ apt-get upgrade -y
 wget "$URL" -qO file.deb && sudo dpkg -i file.deb; rm file.deb
 
 chef-server-ctl reconfigure
-chef-server-ctl user-create $USER Stephen Burke steburke71@hotmail.com $PW --filename ~/$USER.pem
+chef-server-ctl user-create $USER $FIRSTNAME $LASTNAME $EMAIL $PW --filename ~/$USER.pem
 chef-server-ctl org-create $ORG1 $ORG2 --association_user $USER --filename ~/$USER.pem
 chef-server-ctl install chef-manage
 chef-server-ctl reconfigure
