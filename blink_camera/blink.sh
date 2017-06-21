5a3d26ed18b470d102ea89238a511db1ae8518b5
Stephen Burke <sburke@thunderhead.com>
2017-06-21 12:03:13 -0400

    fixed typo

--- blink_camera/blink.sh
+++ blink_camera/blink.sh
@@ -32,15 +32,18 @@ else
 	fi
 	fi
 
-	NETWORKID=$(curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/networks | grep -o '\"summary\":{\".\{0,4\}' | cut -c13-)
+	NETWORKID=$(curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/networks | grep -o '\"summary\":{\".\{0,6\}' | cut -c13-)
 
 	if [ "$1" == "armedstatus" ]; then
 		curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/homescreen | grep  -oh armed\":"\w*" | cut -c8- | head -1
+		exit
+	elif [ "$1" == "cameras" ]; then
+		curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/network/${NETWORKID}/cameras | jq -C 
 		exit
 	elif [ "$1" == "unwatched" ]; then
 		curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/api/v2/videos/unwatched | jq -C 
 		exit
 	else
-	echo "Options currently are: { unwatched, armedstatus }"
+	echo "Options currently are: { unwatched, armedstatus, cameras }"
 	fi
 fi
