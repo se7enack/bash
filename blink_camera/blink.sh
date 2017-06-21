e81fd6e85826c66460764ba1f63e43e6700f5b2b
Stephen Burke <sburke@thunderhead.com>
2017-06-21 14:30:41 -0400

    blink.sh

--- blink_camera/blink.sh
+++ blink_camera/blink.sh
@@ -4,7 +4,7 @@
 BLINKKEYSTORE=".blink"
 URL="prod.immedia-semi.com"
 
-prereq () {
+preReq () {
 	if ! [ -x "$(command -v jq)" ]; then
 		clear
 		echo
@@ -16,9 +16,8 @@ prereq () {
 		exit
 	fi
 }
-prereq
 
-if [ ! -d ~/${BLINKKEYSTORE} ]; then
+mkStore () {
 	mkdir ~/${BLINKKEYSTORE}
 	echo null > ~/${BLINKKEYSTORE}/authcode
 	echo
@@ -31,13 +30,14 @@ if [ ! -d ~/${BLINKKEYSTORE} ]; then
 	echo ${PASSWORD} >> ~/${BLINKKEYSTORE}/creds
 	echo
 	echo "Creds Saved to ~/${BLINKKEYSTORE}/"
-else
+}
+
+getStore () {
 	EMAIL=$(sed -n '1p' ~/${BLINKKEYSTORE}/creds)
 	PASSWORD=$(sed -n '2p' ~/${BLINKKEYSTORE}/creds)
 	AUTHCODE=$(cat ~/${BLINKKEYSTORE}/authcode)
-
-	currentauthtest=$(curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/homescreen | grep -o '\"message\":\".\{0,12\}' | cut -c12-)
-	if [ "$currentauthtest" == "Unauthorized" ]; then 
+	AUTHTEST=$(curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/homescreen | grep -o '\"message\":\".\{0,12\}' | cut -c12-)
+	if [ "${AUTHTEST}" == "Unauthorized" ]; then 
 		curl -s -H "Host: ${URL}" -H "Content-Type: application/json" --data-binary '{ "password" : "'"${password}"'", "client_specifier" : "iPhone 9.2 | 2.2 | 222", "email" : "'"${email}"'" }' --compressed https://${URL}/login | grep -o '\"authtoken\":\".\{0,22\}' | cut -c14-  > ~/${BLINKKEYSTORE}/authcode
 		authcode=$(cat ~/${BLINKKEYSTORE}/authcode)
 	if [ "${AUTHCODE}" == "" ]; then
@@ -45,19 +45,30 @@ else
 		exit
 	fi
 	fi
-
 	NETWORKID=$(curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/networks | grep -o '\"summary\":{\".\{0,6\}' | cut -c13-)
+}
 
-	if [ "$1" == "armedstatus" ]; then
-		curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/homescreen | grep  -oh armed\":"\w*" | cut -c8- | head -1
-		exit
-	elif [ "$1" == "cameras" ]; then
-		curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/network/${NETWORKID}/cameras | jq -C 
-		exit
-	elif [ "$1" == "unwatched" ]; then
-		curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/api/v2/videos/unwatched | jq -C 
-		exit
-	else
+
+helpMe () {
 	echo "Options currently are: { unwatched, armedstatus, cameras }"
-	fi
+}
+
+
+preReq
+if [ ! -d ~/${BLINKKEYSTORE} ]; then
+	mkStore
+else
+	getStore
+fi
+if [ "$1" == "armedstatus" ]; then
+	curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/homescreen | grep  -oh armed\":"\w*" | cut -c8- | head -1
+	exit
+elif [ "$1" == "cameras" ]; then
+	curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/network/${NETWORKID}/cameras | jq -C 
+	exit
+elif [ "$1" == "unwatched" ]; then
+	curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/api/v2/videos/unwatched | jq -C 
+	exit
+else
+helpMe
 fi
