01e0294f8dcfc1e42f1b7dae61fc453fadfe50b1
Stephen Burke <sburke@thunderhead.com>
2017-06-21 22:03:05 -0400

    blink.sh

--- blink_camera/blink.sh
+++ blink_camera/blink.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Folder in ~ for Blink auth/cred files to be saved
-BLINKKEYSTORE=".blink"
 URL="prod.immedia-semi.com"
+BLINKDIR=".blink"
 
 preReq () {
 	if ! [ -x "$(command -v jq)" ]; then
@@ -17,29 +17,29 @@ preReq () {
 	fi
 }
 
-mkStore () {
-	mkdir ~/${BLINKKEYSTORE}
-	echo null > ~/${BLINKKEYSTORE}/authcode
-	echo
-	echo "Enter your username <email>:"
-	read email
-	echo $email > ~/${BLINKKEYSTORE}/creds
-	echo 
-	echo "Enter your password:"
-	read password
-	echo ${PASSWORD} >> ~/${BLINKKEYSTORE}/creds
-	echo
-	echo "Creds Saved to ~/${BLINKKEYSTORE}/"
+helpMe () {
+	echo Options are currently limited to: { cameras, unwatched }
 }
 
-getStore () {
-	EMAIL=$(sed -n '1p' ~/${BLINKKEYSTORE}/creds)
-	PASSWORD=$(sed -n '2p' ~/${BLINKKEYSTORE}/creds)
-	AUTHCODE=$(cat ~/${BLINKKEYSTORE}/authcode)
+credGet () {
+	if [ ! -d ~/${BLINKDIR} ]; then
+		mkdir ~/${BLINKDIR}
+		echo null > ~/${BLINKDIR}/authcode
+		echo Enter your username \(email\):
+		read EMAIL
+		echo ${EMAIL} > ~/${BLINKDIR}/creds
+		echo
+		echo Enter your password:
+		read PASSWORD
+		echo ${PASSWORD} >> ~/${BLINKDIR}/creds
+	fi
+	EMAIL=$(sed -n '1p' ~/${BLINKDIR}/creds)
+	PASSWORD=$(sed -n '2p' ~/${BLINKDIR}/creds)
+	AUTHCODE=$(cat ~/${BLINKDIR}/authcode)
 	AUTHTEST=$(curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/homescreen | grep -o '\"message\":\".\{0,12\}' | cut -c12-)
 	if [ "${AUTHTEST}" == "Unauthorized" ]; then 
-		curl -s -H "Host: ${URL}" -H "Content-Type: application/json" --data-binary '{ "password" : "'"${password}"'", "client_specifier" : "iPhone 9.2 | 2.2 | 222", "email" : "'"${email}"'" }' --compressed https://${URL}/login | grep -o '\"authtoken\":\".\{0,22\}' | cut -c14-  > ~/${BLINKKEYSTORE}/authcode
-		authcode=$(cat ~/${BLINKKEYSTORE}/authcode)
+		curl -s -H "Host: ${URL}" -H "Content-Type: application/json" --data-binary '{ "password" : "'"${PASSWORD}"'", "client_specifier" : "iPhone 9.2 | 2.2 | 222", "email" : "'"${EMAIL}"'" }' --compressed https://${URL}/login | grep -o '\"authtoken\":\".\{0,22\}' | cut -c14-  > ~/${BLINKDIR}/authcode
+		AUTHCODE=$(cat ~/${BLINKDIR}/authcode)
 	if [ "${AUTHCODE}" == "" ]; then
 		echo "No Authcode received, please check credentials"
 		exit
@@ -48,27 +48,14 @@ getStore () {
 	NETWORKID=$(curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/networks | grep -o '\"summary\":{\".\{0,6\}' | cut -c13-)
 }
 
-
-helpMe () {
-	echo "Options currently are: { unwatched, armedstatus, cameras }"
-}
-
-
-preReq
-if [ ! -d ~/${BLINKKEYSTORE} ]; then
-	mkStore
-else
-	getStore
-fi
-if [ "$1" == "armedstatus" ]; then
-	curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/homescreen | grep  -oh armed\":"\w*" | cut -c8- | head -1
-	exit
-elif [ "$1" == "cameras" ]; then
+preReq;credGet
+if [ "$1" == "cameras" ]; then
 	curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/network/${NETWORKID}/cameras | jq -C 
 	exit
 elif [ "$1" == "unwatched" ]; then
 	curl -s -H "Host: ${URL}" -H "TOKEN_AUTH: ${AUTHCODE}" --compressed https://${URL}/api/v2/videos/unwatched | jq -C 
 	exit
 else
-helpMe
+	helpMe
 fi
+
