e3ee7794b88b28f4899dbac4d4ec587ab21ecb07
Stephen Burke <sburke@thunderhead.com>
2017-07-26 16:22:16 -0400

    added helpful comments

--- private_git_server/gitserver.sh
+++ private_git_server/gitserver.sh
@@ -3,13 +3,16 @@
 # * This was created for and tested on Ubuntu
 
 REPO="acme-company/git-repo-name"
+SERVERIP=$(hostname -I | sed -e 's/[[:space:]]*$//')
+USERNAME=$(w | awk '{ print $1 }' | tail -1)
 
 publicKeyHelp() {
   echo
   echo "Before you start:"
   echo "Please scp your local public key (id_rsa.pub) up to /tmp on the server"
   echo
-  echo "Example: scp ~/.ssh/id_rsa.pub `w | awk '{ print $1 }' | tail -1`@`hostname -I | sed -e 's/[[:space:]]*$//'`:/tmp/id_rsa.pub"
+  echo "Example:"
+  echo "scp ~/.ssh/id_rsa.pub ${USERNAME}@${SERVERIP}:/tmp/id_rsa.pub"
   echo
 }
 
@@ -24,16 +27,26 @@ if [[ $? -ne 0 ]]; then
    exit 1
 fi
 
-su -c "useradd git -s /bin/bash -m"
-apt-get update
-apt-get install -y git
-mkdir -p /srv/${REPO}.git
-chown git /srv/${REPO}.git
-chown git /tmp/id_rsa.pub
-ASGIT="sudo -H -u git bash -c ${1}"
-${ASGIT} "cd"
-${ASGIT} "mkdir -p /home/git/.ssh"
-${ASGIT} "cat /tmp/id_rsa.pub >> /home/git/.ssh/authorized_keys"
-${ASGIT} "cd /srv/${REPO}.git; git init --bare"
-echo "$(which git-shell)" >> /etc/shells
+su -c "useradd git -s /bin/bash -m" && \
+apt-get update && \
+apt-get install -y git && \
+mkdir -p /srv/${REPO}.git && \
+chown git /srv/${REPO}.git && \
+chown git /tmp/id_rsa.pub && \
+ASGIT="sudo -H -u git bash -c ${1}" && \
+${ASGIT} "cd" && \
+${ASGIT} "mkdir -p /home/git/.ssh" && \
+${ASGIT} "cat /tmp/id_rsa.pub >> /home/git/.ssh/authorized_keys" && \
+${ASGIT} "cd /srv/${REPO}.git; git init --bare" && \
+echo "$(which git-shell)" >> /etc/shells && \
 chsh git -s $(which git-shell)
+
+if [[ $? -eq 0 ]]; then
+  clear
+  echo
+  echo "Complete!"
+  echo 
+  echo "From your local machine run:"
+  echo "git clone git@${SERVERIP}:/srv/${REPO}.git"
+  echo
+fi
