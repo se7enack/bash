2f1a361820e979457d3968851fccc56c105096a5
Stephen Burke <se7enack@users.noreply.github.com>
2019-08-29 14:08:54 -0400

    Create putvarsinkeyvault.sh

---
+++ putvarsinkeyvault.sh
@@ -0,0 +1,20 @@
+  
+#!/bin/bash
+instance=${1}
+echo ${instance}
+dc="US"
+input="keys_"${instance}".properties"
+tmpfile=$(mktemp /tmp/${instance}_keyvault_XXX)
+mkdir -p ps1
+touch -a ${input}
+echo "Set-AzContext -Subscription '{AZURE_SUBSCRIPTION}'" > ./ps1/${instance}.ps1
+while IFS= read -r line
+do
+  var="$( cut -d '=' -f 1 <<< "$line" )"
+  varfix="${var//\_/-}"
+  value="$( cut -d '=' -f 2- <<< $line )"
+  echo "$"${var} " = ConvertTo-SecureString -String " ${value} " -AsPlainText -Force" >> ./ps1/${instance}.ps1
+  echo "Set-AzureKeyVaultSecret -VaultName "${instance}-secrets" -Name "${varfix}" -SecretValue $"${var} >> ./ps1/${instance}.ps1
+  echo
+done < "${tmpfile}"
+rm ${tmpfile}
