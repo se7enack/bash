5eb1d76017173383588b5771d71fb810ff7e7a49
Stephen Burke <se7enack@users.noreply.github.com>
2019-09-01 16:41:30 -0400

    Update and rename putvarsinkeyvault.sh to off2keyvault.sh

--- off2keyvault.sh
+++ off2keyvault.sh
@@ -1,7 +1,7 @@
   
 #!/bin/bash
 # This will create a keyvault in Azure and populate it with your key value pairs from a file.
-# Usage: ./putvarsinkeyvault.sh mykeyvaluepairs.properties 
+# Usage: ./off2keyvault.sh mykeyvaluepairs.properties 
 
 propertiesfile=${1}
 subscriptionid="PUT_YOUR_AZURE_SUBSCRIPTION_ID_HERE"
@@ -15,11 +15,12 @@ echo "New-AzureRmKeyVault -VaultName ${keyvaultname} -ResourceGroupName ${resour
 
 while IFS= read -r line
 do
-  var="$( cut -d '=' -f 1 <<< "$line" )"
-  varfix="${var//\_/-}"
+  key="$( cut -d '=' -f 1 <<< "$line" )"
+# keyfix converts underscores to dashes in key names. Azure doesn't support naming a key with an underscore. 
+  keyfix="${key//\_/-}"
   value="$( cut -d '=' -f 2- <<< $line )"
-  echo "$"${var} " = ConvertTo-SecureString -String " ${value} " -AsPlainText -Force" >> ./ps1/${keyvaultname}.ps1
-  echo "Set-AzureKeyVaultSecret -VaultName "${keyvaultname}" -Name "${varfix}" -SecretValue $"${var} >> ./ps1/${keyvaultname}.ps1
+  echo "$"${key} " = ConvertTo-SecureString -String " ${value} " -AsPlainText -Force" >> ./ps1/${keyvaultname}.ps1
+  echo "Set-AzureKeyVaultSecret -VaultName "${keyvaultname}" -Name "${keyfix}" -SecretValue $"${key} >> ./ps1/${keyvaultname}.ps1
   echo
 done < "${propertiesfile}"
 
