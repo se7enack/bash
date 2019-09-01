4d55508214cd343dbc26c3dc954e096e37afc48b
Stephen Burke <se7enack@users.noreply.github.com>
2019-09-01 16:28:49 -0400

    Update putvarsinkeyvault.sh

--- putvarsinkeyvault.sh
+++ putvarsinkeyvault.sh
@@ -1,20 +1,23 @@
   
 #!/bin/bash
-instance=${1}
-echo ${instance}
-dc="US"
-input="keys_"${instance}".properties"
-tmpfile=$(mktemp /tmp/${instance}_keyvault_XXX)
+propertiesfile=${1}
+subscriptionid="PUT_YOUR_AZURE_SUBSCRIPTION_ID_HERE"
+location="eastus2"
+keyvaultname="foobar"
+pscmd=$(which pwsh)
+
 mkdir -p ps1
-touch -a ${input}
-echo "Set-AzContext -Subscription '{AZURE_SUBSCRIPTION}'" > ./ps1/${instance}.ps1
+echo "Set-AzContext -Subscription ${subscriptionid}" > ./ps1/${keyvaultname}.ps1
+echo "New-AzureRmKeyVault -VaultName ${keyvaultname} -ResourceGroupName ${resourcegroupname} -Location ${location}" >> ./ps1/${keyvaultname}.ps1
+
 while IFS= read -r line
 do
   var="$( cut -d '=' -f 1 <<< "$line" )"
   varfix="${var//\_/-}"
   value="$( cut -d '=' -f 2- <<< $line )"
-  echo "$"${var} " = ConvertTo-SecureString -String " ${value} " -AsPlainText -Force" >> ./ps1/${instance}.ps1
-  echo "Set-AzureKeyVaultSecret -VaultName "${instance}-secrets" -Name "${varfix}" -SecretValue $"${var} >> ./ps1/${instance}.ps1
+  echo "$"${var} " = ConvertTo-SecureString -String " ${value} " -AsPlainText -Force" >> ./ps1/${keyvaultname}.ps1
+  echo "Set-AzureKeyVaultSecret -VaultName "${keyvaultname}" -Name "${varfix}" -SecretValue $"${var} >> ./ps1/${keyvaultname}.ps1
   echo
-done < "${tmpfile}"
-rm ${tmpfile}
+done < "${propertiesfile}"
+
+${pscmd} ./ps1/${keyvaultname}.ps1
