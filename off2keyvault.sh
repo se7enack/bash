7ade2d7e4f254533930674bae5c566ca0350eea3
Stephen Burke <se7enack@users.noreply.github.com>
2019-09-01 17:20:37 -0400

    Update off2keyvault.sh

--- off2keyvault.sh
+++ off2keyvault.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This will create a keyvault in Azure and populate it with your key value pairs from a file.
+# This will create a powershell script (in child directory ps1/) that will create a keyvault in Azure and populate it with your key value pairs from file.
 # Usage: ./off2keyvault.sh mykeyvaluepairs.properties 
 
 subscriptionid="PUT_YOUR_AZURE_SUBSCRIPTION_ID_HERE"
@@ -9,6 +9,7 @@ keyvaultname="foobar"
 mkdir -p ps1
 echo "Set-AzContext -Subscription ${subscriptionid}" > ./ps1/${keyvaultname}.ps1
 echo "New-AzureRmKeyVault -VaultName ${keyvaultname} -ResourceGroupName ${resourcegroupname} -Location ${location}" >> ./ps1/${keyvaultname}.ps1
+echo "" >> ./ps1/${keyvaultname}.ps1
 
 while IFS= read -r line
 do
@@ -18,5 +19,7 @@ do
   value="$( cut -d '=' -f 2- <<< $line )"
   echo "$"${key} " = ConvertTo-SecureString -String " ${value} " -AsPlainText -Force" >> ./ps1/${keyvaultname}.ps1
   echo "Set-AzureKeyVaultSecret -VaultName "${keyvaultname}" -Name "${keyfix}" -SecretValue $"${key} >> ./ps1/${keyvaultname}.ps1
-  echo
+  echo "" >> ./ps1/${keyvaultname}.ps1
 done < "${1}"
+
+echo; echo "Your powershell script is here: ./ps1/${keyvaultname}.ps1";echo
