e39dcfa9e7b2ba73d5419cd3faf5af0c03866f5f
Stephen Burke <se7enack@users.noreply.github.com>
2019-10-01 16:02:48 -0400

    Update off2keyvault.sh

--- off2keyvault.sh
+++ off2keyvault.sh
@@ -14,11 +14,13 @@ echo "" >> ./ps1/${keyvaultname}.ps1
 while IFS= read -r line
 do
   key="$( cut -d '=' -f 1 <<< "$line" )"
-# keyfix converts underscores to dashes in key names. Azure doesn't support naming a key with an underscore. 
+# keyfix converts underscores to dashes in key names. Azure doesn't support naming a key with an underscore.
   keyfix="${key//\_/-}"
+# keyfix2 converts dashes to underscores in key for azure varnames. Azure doesn't support naming a key with a dash (I know, right?).
+  keyfix2="${key//\-/_}"
   value="$( cut -d '=' -f 2- <<< $line )"
-  echo "$"${key} " = ConvertTo-SecureString -String " ${value} " -AsPlainText -Force" >> ./ps1/${keyvaultname}.ps1
-  echo "Set-AzureKeyVaultSecret -VaultName "${keyvaultname}" -Name "${keyfix}" -SecretValue $"${key} >> ./ps1/${keyvaultname}.ps1
+  echo "$"${keyfix2} " = ConvertTo-SecureString -String " ${value} " -AsPlainText -Force" >> ./ps1/${keyvaultname}.ps1
+  echo "Set-AzureKeyVaultSecret -VaultName "${keyvaultname}" -Name "${keyfix}" -SecretValue $"${keyfix2} >> ./ps1/${keyvaultname}.ps1
   echo "" >> ./ps1/${keyvaultname}.ps1
 done < "${1}"
 
