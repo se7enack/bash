030b3882311d829d377071d684c5f951f76e7653
Stephen Burke <se7enack@users.noreply.github.com>
2020-10-28 13:17:01 -0400

    Update cidrblock.sh

--- cidrblock.sh
+++ cidrblock.sh
@@ -1,8 +1,12 @@
 x=1
-echo;echo "CIDR   BLOCK";echo
+echo;echo "CIDR                    SUBNET                 BLOCK";echo
 for i in {32..08}
 do
- echo "/$(printf %02d $i)    $x"
- x=$((x*2))
+  cidr=$(printf %02d $i)
+  s=$(( 0xffffffff ^ ((1 << (32-$i)) -1) ))
+  sn=$(( (s>>24) & 0xff )).$(( (s>>16) & 0xff )).$(( (s>>8) & 0xff )).$(( s & 0xff ))
+  line='                     '
+  printf "%s %s %s %s \n" /$cidr "${line:${#cidr}} $sn ${line:${#sn}} $x ${line:${#x}}"
+  x=$((x*2))
 done | sort
 echo
