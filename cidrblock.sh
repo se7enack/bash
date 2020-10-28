d57d09141bff09c1330fef15d6b1dd08f17af308
Stephen Burke <se7enack@users.noreply.github.com>
2020-10-28 15:48:49 -0400

    Update cidrblock.sh

--- cidrblock.sh
+++ cidrblock.sh
@@ -1,14 +1,21 @@
 x=1
-echo;echo "CIDR                    SUBNET                 BLOCK                 INCREMENT";echo
+echo;echo "CIDR                    SUBNET                 BLOCK                 INCREMENT              CLASS";echo
 for i in {32..08}
 do
+  if [[ $i -ge 24 ]]; then
+  	class="C"
+  elif [[ $i -ge 16 ]]; then
+  	class="B"
+  else
+  	class="A"
+  fi
   cidr=$(printf %02d $i)
   s=$(( 0xffffffff ^ ((1 << (32-$i)) -1) ))
   sn=$(( (s>>24) & 0xff )).$(( (s>>16) & 0xff )).$(( (s>>8) & 0xff )).$(( s & 0xff ))
   math=$(echo $sn  | sed 's/\.0//' | sed 's/\.0//'  | sed 's/\.0//' | awk -F '.' '{print $(NF)}')
   inc=$((256-$math))
   line='                     '
-  printf "%s %s %s %s %s\n" "/$cidr" "${line:${#cidr}} $sn ${line:${#sn}} $x ${line:${#x}} $inc ${line:${#inc}}"
+  printf "%s %s %s %s %s %s\n" "/$cidr" "${line:${#cidr}} $sn ${line:${#sn}} $x ${line:${#x}} $inc ${line:${#inc}} $class ${line:${#class}}"
   x=$((x*2))
 done | sort 
 echo
