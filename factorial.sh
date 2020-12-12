3243ff46844d0e54ab3b6b8c3968f40d65bc60a0
Stephen Burke <se7enack@users.noreply.github.com>
2020-12-12 16:38:59 -0500

    Update factorial.sh

--- factorial.sh
+++ factorial.sh
@@ -4,22 +4,23 @@ COUNTER=1
 re='^[0-9]+$'
 
 factorials() {
-    if (( $1 <= 1 )); then
+    if (( ${1} <= 1 )); then
         echo 1
     else
-        last=$(factorials $(( $1 - 1 )))
-        echo $(( $1 * last ))
+        last=$(factorials $(( ${1} - 1 )))
+        echo $(( ${1} * last ))
     fi
 }
 
-if ! [[ $1 =~ $re ]] || [[ $1 > 20 ]]; then
-   echo "This excepts integers between 1 and 20 only" >&2; exit 1
+if ! [[ ${1} =~ ${re} ]] || [[ ${1} > 20 ]]; then
+   echo -e "\nThis excepts integers between 1 and 20 only" 
+   echo -e "Example: ${0} [1-20]\n" >&2; exit 1
 else
-	COUNT=$(( $1+1 ))
+	COUNT=$(( ${1}+1 ))
 fi
 
-while [ $COUNTER -lt $COUNT ]; do
-	factorials $COUNTER
+while [ ${COUNTER} -lt ${COUNT} ]; do
+	factorials ${COUNTER}
 	let COUNTER=COUNTER+1 
 done
-     
+      
