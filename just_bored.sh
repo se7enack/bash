72c931f0558cb1519acb5bf9d584e5af061bc1fa
Stephen Burke <se7enack@users.noreply.github.com>
2020-12-12 21:39:33 -0500

    Create just_bored.sh

---
+++ just_bored.sh
@@ -0,0 +1,30 @@
+!/bin/bash
+
+#RE: https://q4interview.com/aptitude-ques-ans-discussion.php?qid=4416&t=70&qnum=346&cat=29
+
+counter=7
+count=0
+a=1
+b=${a}
+c=2
+
+fun() {
+	echo $a
+	count=$(($count+1))
+	echo $b
+	a=$(($a*$b+$count))
+	echo $a
+	count=$(($count+1))
+	while [ ${counter} -gt 1 ]; do
+		b=$(($a))
+		a=$(($a*$b+$count))
+		echo $(($a-$c))
+		c=$(($c+1))
+		echo $a
+		count=$(($count+1))
+		counter=$(($counter-1))
+	done
+}
+
+fun
+  
