3f58c46407de6442cc18762bb33f51597384a079
Stephen Burke <steburke71@hotmail.com>
2022-09-11 17:32:16 -0400

    Update email.sh

--- email.sh
+++ email.sh
@@ -30,15 +30,6 @@ select option in "${options[@]}"; do
             echo "${option}ing...";echo
             numeq=${msg: -1}
             base=$(echo -n ${msg} | rev | cut -c2- | rev)
-            if [ $? == 0 ]; then
-                echo -n ${solve} | base64 -D
-                while [ $numeq -gt 0 ]; do
-                        base="${base}="
-                        let numeq=numeq-1
-                done
-            else
-                echo "Incorrect encryption key provided. Please try again."
-            fi
             solve=$(echo ${base} | base64 -D 2> /dev/null | openssl aes-256-cbc -d -a -pass pass:${key} 2> /dev/null)
             if [ $? == 0 ]; then
                 echo ${solve} | base64 -D
