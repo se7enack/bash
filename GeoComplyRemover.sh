30714f4465b27b44da717fe07e7063ad20c7ea55
Stephen Burke <steburke71@hotmail.com>
2023-05-11 16:44:36 -0400

    GeoComply Remover

--- GeoComplyRemover.sh
+++ GeoComplyRemover.sh
@@ -28,14 +28,14 @@ if [ $onecount -gt 1 ] || [ $twocount -gt 1 ]; then
   if [ $? -eq 0 ]; then
     osascript <<EOT
       tell app "System Events"
-        display dialog "$1" buttons {"OK"} default button 1 with icon caution with title "Removed"
+        display dialog "$1" buttons {"OK"} default button 1 with icon note with title "Removed"
         return  -- Suppress result
       end tell
 EOT
   else
     osascript <<EOT
       tell app "System Events"
-        display dialog "$1" buttons {"OK"} default button 1 with icon caution with title "Failed"
+        display dialog "$1" buttons {"OK"} default button 1 with icon stop with title "Failed"
         return  -- Suppress result
       end tell
 EOT
