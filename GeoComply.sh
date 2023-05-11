d82e31f4b4f4e9eda0dffcdd3c5b5e4e4c873170
Stephen Burke <steburke71@hotmail.com>
2023-05-11 15:11:34 -0400

    Update GeoComply.sh

--- GeoComply.sh
+++ GeoComply.sh
@@ -2,9 +2,16 @@
 
 #run this as sudo
 
-kill $(ps -aef | grep PlayerLo | head -1 | awk '{print $2}')
-kill $(ps -aef | grep GeoComply | head -1 | awk '{print $2}')
-mkdir -p /tmp/GeoComply
-mv /Library/Application\ Support/GeoComply /tmp/GeoComply/.
-mv /Applications/PlayerLocationCheck.app /tmp/GeoComply/.
+kill $(ps -aef | grep PlayerLo | head -1 | awk '{print $2}');\
+kill $(ps -aef | grep GeoComply | head -1 | awk '{print $2}');\
+mkdir -p /tmp/GeoComply;\
+mv /Library/Application\ Support/GeoComply /tmp/GeoComply/.;\
+mv /Applications/PlayerLocationCheck.app /tmp/GeoComply/.;\
 rm -rf /tmp/GeoComply
+
+osascript <<EOT
+  tell app "System Events"
+    display dialog "$1" buttons {"OK"} default button 1 with icon caution with title "Removed"
+    return  -- Suppress result
+  end tell
+EOT
