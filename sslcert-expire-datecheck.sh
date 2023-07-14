97ac75a4f960012b949b2f645929ef65f82a1113
Stephen Burke <se7enack@users.noreply.github.com>
2023-07-14 09:49:32 -0400

    sslcert-expire-datecheck.sh
    checks when an ssl cert will expire

---
+++ sslcert-expire-datecheck.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo "curl --insecure -vvI https://${1} 2>&1 | awk 'BEGIN { cert=0 } /^\* SSL connection/ { cert=1 } /^\*/ { if (cert) print }' | grep expire|awk -F \":\" '{print $2 \"\" $4}' | awk '{print $1,$2,$4}'" >/usr/local/bin/expires
+
+echo "Installed!"
+echo "  Usage:  'expires github.com'"
+echo "  Result: 'Mar 14 2024'";echo
